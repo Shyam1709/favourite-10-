d0494339e3932510d196b58ef82eac6ecde4a0c0
shyam1709 <shyam@knightlearningsolutions.com>
2018-03-14 16:07:18 +0530

    fav-10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { RouterModule, Routes } from '@angular/router';
 
-import{ HomeComponent } from './components/home/home.component';
-import{ SearchMovieComponent} from './components/search-movie/search-movie.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchMovieComponent } from './components/search-movie/search-movie.component';
+import { FavouriteMoviesComponent } from './components/favourite-movies/favourite-movies.component';
 
 const routes :Routes = [
   {path: '', redirectTo:'/home',pathMatch:'full'},
   {path: 'home', component:HomeComponent},
-  {path: '', },
+  {path: 'favourite', component:FavouriteMoviesComponent },
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {TmdbApiService} from './services/tmdb-api.service';
+import { TmdbApiService } from './services/tmdb-api.service';
+import { JsonApiService } from './services/json-api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers:[TmdbApiService]
+  providers:[TmdbApiService,JsonApiService]
 })
 export class AppComponent {
   title = 'app';
